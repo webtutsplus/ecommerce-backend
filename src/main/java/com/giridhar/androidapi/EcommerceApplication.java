cf3dabde9711601a45ddc37c49575763787cfe28
giridhar30 <giriit30@gmail.com>
2020-12-09 21:42:11 +0530

    initial commit

---
+++ src/main/java/com/giridhar/androidapi/EcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.giridhar.androidapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+}
