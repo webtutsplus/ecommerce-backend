0fe78ca39d0fbbafc643ff42ae19bfc29b5f99f2
giridhar30 <giriit30@gmail.com>
2020-12-13 23:43:57 +0530

    commit after category api completion

--- src/main/java/com/webtutsplus/ecommerce/controller/CategoryController.java
+++ src/main/java/com/webtutsplus/ecommerce/controller/CategoryController.java
@@ -1,22 +1,35 @@
 package com.webtutsplus.ecommerce.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webtutsplus.ecommerce.model.Category;
-import com.webtutsplus.ecommerce.services.CategoryService;
+import com.webtutsplus.ecommerce.service.CategoryService;
 
 @RestController
-public class APIController {
+@RequestMapping("/category")
+public class CategoryController {
 	
 	@Autowired
 	private CategoryService cs;
 	
-	@PostMapping("/category/create")
+	@GetMapping("/")
+    public ResponseEntity<List<Category>> getProducts() {
+        List<Category> body = cs.listCategories();
+        return new ResponseEntity<List<Category>>(body, HttpStatus.OK);
+    }
+	
+	@PostMapping("/create")
 	public String createCategory(@Valid @RequestBody Category category) {
 		if (cs.readCategory(category.getCategoryName()) != null) {
 			return "CATEGORY_ALREADY_EXISTS";
@@ -26,7 +39,7 @@ public class APIController {
 		return "SUCCESS";
 	}
 	
-	@PostMapping("/category/read")
+	@PostMapping("/read")
 	public Category readCategory(@RequestBody long categoryId) {
 		return cs.readCategory(categoryId);
 	}
