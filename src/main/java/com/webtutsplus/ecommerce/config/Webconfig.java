d378142bd85f5ddd41705725f3d97805513f09d9
nmadhab <nilmadhab1994@booking.com>
2020-12-16 19:19:59 +0000

    cors added

--- src/main/java/com/webtutsplus/ecommerce/config/Webconfig.java
+++ src/main/java/com/webtutsplus/ecommerce/config/Webconfig.java
@@ -1,16 +1,23 @@
 package com.webtutsplus.ecommerce.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-class WebConfig implements WebMvcConfigurer {
+class Webconfig{
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
     }
 }
