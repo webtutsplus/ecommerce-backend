d9b85c7f1c582cbb3dc40418e161f03a97b7536e
Jean-Jacques <jeanjacques.strydom34@gmail.com>
2020-12-30 14:40:15 +0200

    Added more documentation

--- src/main/java/com/webtutsplus/ecommerce/model/Category.java
+++ src/main/java/com/webtutsplus/ecommerce/model/Category.java
@@ -7,7 +7,6 @@ import java.util.Set;
 @Entity
 @Table(name = "categories")
 public class Category {
-
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -16,45 +15,72 @@ public class Category {
 	private @NotBlank String categoryName;
 
 	private @NotBlank String description;
-
 	private @NotBlank String imageUrl;
-
-	// add imageURL here
 
 	@OneToMany(mappedBy = "category", fetch = FetchType.LAZY,
 			cascade = CascadeType.ALL)
 	Set<Product> products;
 	
-	public Category() {
-	}
-	
+	public Category() {}
+
+	/**
+	 * Constructor for the Category object.
+	 * @param categoryName The name of the category.
+	 * @param description A description of the category.
+	 */
 	public Category(@NotBlank String categoryName, @NotBlank String description) {
 		this.categoryName = categoryName;
 		this.description = description;
 	}
 
+	/**
+	 * Constructor for the Category object.
+	 * @param categoryName The name of the category.
+	 * @param description A description of the category.
+	 * @param imageUrl An image to describe the category.
+	 */
 	public Category(@NotBlank String categoryName, @NotBlank String description, @NotBlank String imageUrl) {
 		this.categoryName = categoryName;
 		this.description = description;
 		this.imageUrl = imageUrl;
 	}
 
+	/**
+	 * Used to get the name of the category.
+	 * @return category name of type String.
+	 */
 	public String getCategoryName() {
 		return categoryName;
 	}
 
+	/**
+	 * Used to set the name of the category.
+	 * @param categoryName the new name of the category.
+	 */
 	public void setCategoryName(String categoryName) {
 		this.categoryName = categoryName;
 	}
 
+	/**
+	 * Used to get the description of the category.
+	 * @return category description of type String.
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Used to set the description of the category.
+	 * @param description the new description of the category.
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
+	/**
+	 * Used to get the set of all products that are apart of the category.
+	 * @return set of products.
+	 */
 	public Set<Product> getProducts() {
 		return products;
 	}
@@ -68,6 +94,10 @@ public class Category {
 		return "User {category id=" + id + ", category name='" + categoryName + "', description='" + description + "'}";
 	}
 
+	/**
+	 * Used to get the image URL of the category.
+	 * @return image URL of type String.
+	 */
 	public String getImageUrl() {
 		return imageUrl;
 	}
@@ -76,6 +106,10 @@ public class Category {
 		this.imageUrl = imageUrl;
 	}
 
+	/**
+	 * Used to get the ID of the category.
+	 * @return category ID of type Long.
+	 */
 	public Long getId() {
 		return id;
 	}
