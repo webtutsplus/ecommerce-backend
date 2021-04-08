0a7c441e56796479a0e4779caa673c546d411053
nmadhab <nilmadhab1994@booking.com>
2021-04-08 14:20:48 +0530

    heroku base url

--- src/main/java/com/webtutsplus/ecommerce/service/OrderService.java
+++ src/main/java/com/webtutsplus/ecommerce/service/OrderService.java
@@ -34,7 +34,7 @@ public class OrderService {
     @Autowired
     OrderItemsService orderItemsService;
 
-    @Value("${baseURL}")
+    @Value("https://infallible-swartz-b50174.netlify.app")
     private String baseURL;
 
     @Value("${STRIPE_SECRET_KEY}")
